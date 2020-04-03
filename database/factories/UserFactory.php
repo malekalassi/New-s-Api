789e031e917cbd68a5dbe7164999bf10a952d0e2
malek <malekassi3@gmail.com>
2020-04-03 11:44:29 +0300

    ADD api_token to user and user obsever

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'avatar'=>$faker->imageUrl(150,150),
+        'api_token'=>bin2hex(openssl_random_pseudo_bytes(30)),
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
